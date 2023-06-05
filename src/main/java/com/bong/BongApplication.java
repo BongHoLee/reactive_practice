f688277f13c9f74f763ee1db0521c0d37f79dfed
bongholee <leebongho9204@gmail.com>
2023-06-05 19:50:24 +0900

    init

---
+++ src/main/java/com/bong/BongApplication.java
@@ -0,0 +1,13 @@
+package com.bong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BongApplication.class, args);
+    }
+
+}
